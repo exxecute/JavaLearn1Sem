1f8451148ff45feb95ae909b01256d7795d981bb
Uladzislau Mikhayevich <3xecvte@gmail.com>
2024-01-09 16:29:59 +0200

    3/Loops: 11 done

--- Learn3/ExaminatorTasks/Loops/Exercise11/Main.java
+++ Learn3/ExaminatorTasks/Loops/Exercise11/Main.java
@@ -6,20 +6,21 @@ class Main {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) 
         {
-            float n = (float) sc.nextInt();
-            float h = sc.nextFloat();
-
-            double x = 0;
-            
-            for(int i = 0; i < n; i++)
+            int n = sc.nextInt();
+            double h = sc.nextDouble();
+    
+            double sum = 0;
+    
+            for(int i = 0; i < n; i++) 
             {
-                if(i < h)
+                double x = 2 - 10 * Math.sin(3 * i);
+                if(Math.abs(x) < h) 
                 {
-                    x = 2 - 10 * Math.sin(3 * i);
+                    sum += x;
                 }
             }
-
-            System.out.printf("%.3f ", (float) x);
+    
+            System.out.printf("%.3f", sum);
         }
     }
 }
