bfba315d036ce17a185f9661c81adba5f34a173c
Uladzislau Mikhayevich <3xecvte@gmail.com>
2024-01-09 16:28:01 +0200

    3/Loops: 14 done

--- Learn3/ExaminatorTasks/Loops/Exercise14/Main.java
+++ Learn3/ExaminatorTasks/Loops/Exercise14/Main.java
@@ -7,7 +7,14 @@ class Main {
         try (Scanner sc = new Scanner(System.in)) 
         {
             int n = sc.nextInt();
-
+    
+            for(int i = 1; i <= n; i++) 
+            {
+                if(n % i == 0) 
+                {
+                    System.out.printf("%d ", i);
+                }
+            }
         }
     }
 }
