f81ec2b11a2aec275fe716dbf97f9bfbde4bb9a7
Uladzislau Mikhayevich <3xecvte@gmail.com>
2024-01-09 16:24:57 +0200

    3/Loops: 9 done

--- Learn3/ExaminatorTasks/Loops/Exercise9/Main.java
+++ Learn3/ExaminatorTasks/Loops/Exercise9/Main.java
@@ -8,24 +8,19 @@ class Main
         // Locale.setDefault(new Locale("EN"));
         try (Scanner sc = new Scanner(System.in)) 
         {
-            float a, b, n, h;
-
-            a = sc.nextFloat();
-            b = sc.nextFloat();
-            n = sc.nextFloat();
-
-            h = (b - a + 1) / n;
-
-            float x = a;
-
-            System.out.printf("h = %.3f\n", h);
-
-            while(x <= b)
+            double a = sc.nextDouble();
+            double b = sc.nextDouble();
+            int n = sc.nextInt();
+    
+            double h = (b - a) / (n - 1);
+    
+            int i = 0;
+            while(i < n) 
             {
-                double f = 1 + 2 * Math.sin(x);
-                System.out.printf("%.3f ", f);
-                x += h;
-                System.out.println(x);
+                double x = a + i * h;
+                double Fx = 1 + 2 * Math.sin(x);
+                System.out.printf("%.3f ", Fx);
+                i++;
             }
         }
     }    
