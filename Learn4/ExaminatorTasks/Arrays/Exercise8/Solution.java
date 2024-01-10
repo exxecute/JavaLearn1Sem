171c769a6db004dc4cbf9da5876f10bf1b3532da
Uladzislau Mikhayevich <3xecvte@gmail.com>
2024-01-10 10:03:11 +0200

    Learn4/ExaminatorTasks/Arrays/8: done

--- Learn4/ExaminatorTasks/Arrays/Exercise8/Solution.java
+++ Learn4/ExaminatorTasks/Arrays/Exercise8/Solution.java
@@ -7,7 +7,17 @@ class Solution {
      * and k2 is minimum of elements of the array a with odd indices.
      */
     public static int getResult(int[] a) {
-        //put your code here
-        return 0;
+        int k1 = a[0];
+        int k2 = a[1];
+
+        for(int i = 0; i < a.length; i++) {
+            if(i % 2 == 0 && a[i] > k1) {
+                k1 = a[i];
+            } else if(i % 2 != 0 && a[i] < k2) {
+                k2 = a[i];
+            }
+        }
+
+        return k1 + k2;
     }
 }
--- Learn4/ExaminatorTasks/Arrays/Exercise9/Solution.java
+++ Learn4/ExaminatorTasks/Arrays/Exercise9/Solution.java
@@ -1,23 +1,13 @@
 class Solution {
 
     /**
-     * @param a the given array, a.length >= 2
-     * @return k1 + k2,
-     * where k1 is maximum of elements of the array a with even indices,
-     * and k2 is minimum of elements of the array a with odd indices.
+     * @param a the given array
+     * @param c the given number
+     * @param d the given number
+     * @return the number of elements of the array a that belong to the segment [?, d]
      */
-    public static int getResult(int[] a) {
-        int k1 = a[0];
-        int k2 = a[1];
-
-        for(int i = 0; i < a.length; i++) {
-            if(i % 2 == 0 && a[i] > k1) {
-                k1 = a[i];
-            } else if(i % 2 != 0 && a[i] < k2) {
-                k2 = a[i];
-            }
-        }
-
-        return k1 + k2;
+    public static int getResult(int[] a, int c, int d) {
+        //put your code here
+        return 0;
     }
 }
