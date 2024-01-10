fff942c76094eca80366da5b3307e0c5f63ee03d
Uladzislau Mikhayevich <3xecvte@gmail.com>
2024-01-10 10:02:08 +0200

    Learn4/ExaminatorTasks/Arrays/8-9: done

---
+++ Learn4/ExaminatorTasks/Arrays/Exercise8/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+
+    /**
+     * @param a the given array, a.length >= 2
+     * @return k1 + k2,
+     * where k1 is maximum of elements of the array a with even indices,
+     * and k2 is minimum of elements of the array a with odd indices.
+     */
+    public static int getResult(int[] a) {
+        //put your code here
+        return 0;
+    }
+}
