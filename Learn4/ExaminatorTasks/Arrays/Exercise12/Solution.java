715a2af94dfd3aa79139df93751ee327e04158cd
Uladzislau Mikhayevich <3xecvte@gmail.com>
2024-01-10 10:12:02 +0200

    Learn4/ExaminatorTasks/Arrays/11: done

---
+++ Learn4/ExaminatorTasks/Arrays/Exercise12/Solution.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    /**
+     * method compresses the array
+     * by shifting elements with even indices to the first half of the array
+     * and replacing the rest of the array with zeros.
+     *
+     * @param a the given array
+     */
+    public static void compress(int[] a) {
+        
+        int answer = 0;
+
+        for (int i = 0; i < a.length; i++) 
+        {
+            if (i % 2 == 0) 
+            {
+                a[answer] = a[i];
+                answer++;
+            }
+
+            if(i >= answer) 
+            {
+                a[i] = 0;
+            }
+        }
+    }
+}
